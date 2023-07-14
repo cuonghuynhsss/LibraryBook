30ac38f7f91ea71292faeaf3a5a5360c1e5a9e83
cuonghuynhsss <huynhchicuong12a3@gmail.com>
2023-07-15 00:13:57 +0700

    format file

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,25 @@ impl Book {
 }
 
 impl Library {
-    fn new() -> Library{
-        Library{books: Vec::new()}        
+    fn new() -> Library {
+        Library { books: Vec::new() }
     }
 
     fn len(&self) -> usize {
         self.books.len()
     }
-    
-    fn is_empty(&self) -> bool{
+
+    fn is_empty(&self) -> bool {
         self.books.is_empty()
     }
 
-    fn add_book(&mut self, book : Book) {
+    fn add_book(&mut self, book: Book) {
         self.books.push(book)
     }
 
     fn print_books(&self) {
         for book in &self.books {
-            println!("{}, published in {}", book.title, book.year);            
+            println!("{}, published in {}", book.title, book.year);
         }
     }
 
@@ -43,30 +43,35 @@ impl Library {
         let mut oldest: Option<&Book> = None;
         for book in self.books.iter() {
             if oldest.is_none() || book.year < oldest.unwrap().year {
-                oldest = Some(book);                
+                oldest = Some(book);
             }
         }
         oldest
     }
 }
- fn main() {
-     let mut library = Library::new();
+fn main() {
+    let mut library = Library::new();
 
-    println!("The library is empty: library.is_empty() -> {}", library.is_empty());
-    
+    println!(
+        "The library is empty: library.is_empty() -> {}",
+        library.is_empty()
+    );
+
     library.add_book(Book::new("Lord of the Rings", 1954));
     library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
-    
-    println!("The library is no longer empty: library.is_empty() -> {}", library.is_empty());
-    
-    
+
+    println!(
+        "The library is no longer empty: library.is_empty() -> {}",
+        library.is_empty()
+    );
+
     library.print_books();
-    
+
     match library.oldest_book() {
-       Some(book) => println!("The oldest book is {}", book.title),
-       None => println!("The library is empty!"),
+        Some(book) => println!("The oldest book is {}", book.title),
+        None => println!("The library is empty!"),
     }
-    
+
     println!("The library has {} books", library.len());
     library.print_books();
- }
+}
